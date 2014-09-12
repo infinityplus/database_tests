1045bc7de097c3d4926a4e37cccdb575ee20a35f
sasitha <sasithamadushanka@gmail.com>
2014-09-12 11:20:10 +0530

    finished ploting mongodb results

--- server/mongoTestP2_Table1-n.js
+++ server/mongoTestP2_Table1-n.js
@@ -52,6 +52,65 @@ var MongoTestP2_Table1_n = function() {
             }
         });
     };
+    routs['/phase_2_TableMap_1_n_read'] = function(req, res){
+        var query_request_json = req.body;
+        var flight_collection_query  ={
+            "id":query_request_json['id']
+        };
+        mInstant.dbreadone(dburl, "flights_collection", flight_collection_query, function(data){
+            //console.log(data);
+            if(data !== null){
+                var schedule_collection_query = {
+                    "flight_id":data['id']
+                };
+                var customer_collection_query = {
+                    "flight_id":data['id']
+                };
+                var results = {};
+                results["flights_collection"] = data;
+                mInstant.dbreadone(dburl, "schedule_collection", schedule_collection_query, function(shedule_data){
+                    results["schedule_collection"] = shedule_data;
+                    mInstant.dbreadone(dburl, "customer_collection", customer_collection_query, function(customer_data){
+                        results["customer_collection"] = customer_data;
+                        res.statusCode = 200;
+                        res.json(results);
+                    });
+
+                });
+            }else{
+                res.statusCode = 200;
+                res.send("data not found");
+            }
+
+        });
+    };
+    routs['/phase_2_TableMap_1_n_update'] = function(req, res){
+        var flights_collection_json = req.body['flight_collection'];
+        var schedule_collection_json = req.body['schedule_collection'];
+        var customer_collection_json = req.body['customer_collection'];
+
+        var flight_collection_query = {
+            "id":flights_collection_json['id']
+        };
+        mInstant.dbreadone(dburl, "flights_collection", flight_collection_query, function(data){
+            if(data !== null){
+                var schedule_collection_query = {
+                    "flight_id":data['id']
+                };
+                var customer_collection_query = {
+                    "flight_id":data['id']
+                };
+                mInstant.dbupdate(dburl, "schedule_collection", schedule_collection_query, schedule_collection_json, function(){
+                    mInstant.dbupdate(dburl, "customer_collection", customer_collection_query, customer_collection_json, function(){
+                        res.statusCode = 200;
+                        res.send("update successful");
+                    });
+
+                })
+            }
+        });
+
+    };
     return routs;
 
 };
